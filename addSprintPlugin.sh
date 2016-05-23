d5ecbe1c60d465a15ea4a9d9516960d3463aa0bc
root <root@renderman.owndata.net>
2016-05-24 00:45:05 +0200

    hopefully fixed now startup process premission corruption

--- addSprintPlugin.sh
+++ addSprintPlugin.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 # Install Sprint extension
-su git -c 'git clone https://github.com/wikimedia/phabricator-extensions-Sprint.git /srv/phabricator/phabricator-extensions-Sprint'
+git clone https://github.com/wikimedia/phabricator-extensions-Sprint.git /srv/phabricator/phabricator-extensions-Sprint
 
 # enable sprint extension
 cd /srv/phabricator/phabricator/bin
