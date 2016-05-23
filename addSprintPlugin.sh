61014408d702486a25d2f4b5cf1c952218250ef1
root <root@cgserver.owndata.net>
2016-05-24 01:48:07 +0200

    add preamble.php to provide https usage behind reverse proxy isntallation

--- addSprintPlugin.sh
+++ addSprintPlugin.sh
@@ -11,4 +11,10 @@ cd /srv/phabricator/phabricator/bin
 ./config set maniphest.points '{"enabled" : true}'
 ./config set load-libraries '{"sprint":"/srv/phabricator/phabricator-extensions-Sprint/src"}'
 
+# enforce HTTPS detection (for installation behind reverse proxy that adds/removes HTTPS)
+cd /srv/phabricator/phabricator/support
+echo "<?php" > preamble.php
+echo "\$_SERVER['https']=true;" >> preamble.php
+chmod a+rx preamble.php
+
 cd /
